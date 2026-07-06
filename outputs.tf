9eca3bd7168bbc42c23c5abfbc1cd6ca99f90315
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,19 @@
 output "cosmosdb_postgresql_roles" {
   description = "All cosmosdb_postgresql_role resources"
   value       = azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles
+  sensitive   = true
+}
+output "cosmosdb_postgresql_roles_cluster_id" {
+  description = "List of cluster_id values across all cosmosdb_postgresql_roles"
+  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.cluster_id]
+}
+output "cosmosdb_postgresql_roles_name" {
+  description = "List of name values across all cosmosdb_postgresql_roles"
+  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.name]
+}
+output "cosmosdb_postgresql_roles_password" {
+  description = "List of password values across all cosmosdb_postgresql_roles"
+  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.password]
+  sensitive   = true
 }
 
