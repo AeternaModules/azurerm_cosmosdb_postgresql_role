c06356a29c371192368057445bed582f42001983
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "cosmosdb_postgresql_roles_id" {
+  description = "Map of id values across all cosmosdb_postgresql_roles, keyed the same as var.cosmosdb_postgresql_roles"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : k => v.id }
+}
 output "cosmosdb_postgresql_roles_cluster_id" {
   description = "Map of cluster_id values across all cosmosdb_postgresql_roles, keyed the same as var.cosmosdb_postgresql_roles"
   value       = { for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : k => v.cluster_id }
