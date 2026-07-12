53aaf907c7c4a3c4bbcfd27d8cf0daced47da08c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,14 @@
-output "cosmosdb_postgresql_roles" {
-  description = "All cosmosdb_postgresql_role resources"
-  value       = azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles
-  sensitive   = true
-}
 output "cosmosdb_postgresql_roles_cluster_id" {
-  description = "List of cluster_id values across all cosmosdb_postgresql_roles"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.cluster_id]
+  description = "Map of cluster_id values across all cosmosdb_postgresql_roles, keyed the same as var.cosmosdb_postgresql_roles"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : k => v.cluster_id }
 }
 output "cosmosdb_postgresql_roles_name" {
-  description = "List of name values across all cosmosdb_postgresql_roles"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.name]
+  description = "Map of name values across all cosmosdb_postgresql_roles, keyed the same as var.cosmosdb_postgresql_roles"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : k => v.name }
 }
 output "cosmosdb_postgresql_roles_password" {
-  description = "List of password values across all cosmosdb_postgresql_roles"
-  value       = [for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : v.password]
+  description = "Map of password values across all cosmosdb_postgresql_roles, keyed the same as var.cosmosdb_postgresql_roles"
+  value       = { for k, v in azurerm_cosmosdb_postgresql_role.cosmosdb_postgresql_roles : k => v.password }
   sensitive   = true
 }
 
