b566194a830ad2ccb6a2d916d0410adbfd13d426
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:04 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.cosmosdb_postgresql_roles : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_cosmosdb_postgresql_role" "cosmosdb_postgresql_roles" {
   for_each = var.cosmosdb_postgresql_roles
 
   cluster_id = each.value.cluster_id
   name       = each.value.name
-  password   = each.value.password
+  password   = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -5,12 +5,16 @@ Required:
     - cluster_id
     - name
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
 EOT
 
   type = map(object({
-    cluster_id = string
-    name       = string
-    password   = string
+    cluster_id                     = string
+    name                           = string
+    password                       = string
+    password_key_vault_id          = optional(string)
+    password_key_vault_secret_name = optional(string)
   }))
 }
 
